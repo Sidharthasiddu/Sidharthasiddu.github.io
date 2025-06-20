9b5d9879feddc25bf819560049c22fffa1ee82e4
Sidharthasiddu <balagundlasidhartha@gmail.com>
2025-06-20 09:59:22 +0530

    modified:   JavaScript/file1.js 	new file:   JavaScript/file2.js 	new file:   JavaScript/index.html

--- JavaScript/file1.js
+++ JavaScript/file1.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+// console.log("hello world")
+
+let name = "John";
+let age=21;
+let promoted = true;
+let score;
+let city = null;
+
+// console.log(name);
+
+// console.log(typeof name); // string
+// console.log(typeof score); // undefined
+// console.log(typeof age); // number
+// console.log(typeof city); // object
+
+// let strAge = String(age); // number to string
+ 
+// console.log(typeof strAge);
+
+// let rating = "5";
+// console.log(typeof rating);
+
+// let strRating = Number(rating); // string to number
+// console.log(typeof strRating);
+
+let flag = "true";
+let bFlag = Boolean(flag);
+console.log(typeof bFlag); // boolean
---
+++ JavaScript/file2.js
@@ -0,0 +1,39 @@
+// let a =10;
+// let b=20;
+
+// console.log(a+b);
+
+// console.log(a%b);
+
+// console.log(a-b);
+
+// console.log(a/b);
+
+// let c = "10";
+// let d = 30;
+
+// console.log(c+d); // concatenation
+// string to number ==> (multiplication)
+// string to string ==> (addition)  
+// console.log(c*d); // when we are doind multiplication operation string converted to number
+
+// let a=2;
+// let b=2;
+// console.log(a==b);// equality check value
+//  let c= 3;
+//  let d="3";
+//  console.log(c==d);
+//  console.log(c===d);//  Strict equality check value and type
+
+// let a=10;
+// let b=2;
+// let c=12;
+// console.log(a>b);
+// console.log(a>b && a>c); // Logical AND both conditions should be true
+// console.log(a>b || a>c); // Logical OR atleast one condition shoud be true
+
+// let a=10;
+// let b=2;
+// console.log(!a>b); // ! ==> NOT operator
+// console.log(a**b); // exponential (power)
+
